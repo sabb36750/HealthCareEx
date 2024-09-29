59c42ff0a91702a2881f7999741d0fe36b2ab3d0
sab b <sabb36750@gmail.com>
2024-09-29 22:22:44 +0530

    spec repo update

---
+++ src/main/java/com/example/app/repo/SpecializationRepository.java
@@ -0,0 +1,9 @@
+package com.example.app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.app.entity.Specialization;
+
+public interface SpecializationRepository extends JpaRepository<Specialization,Long>{
+
+}
